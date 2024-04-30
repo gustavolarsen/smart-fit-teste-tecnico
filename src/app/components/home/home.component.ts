6c62f853684bded77473519f43c3fe7afca31bda
Gustavo Larsen <guslarsen@gmail.com>
2024-04-29 22:32:15 -0300

    feat: cria componente unit-card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FilterComponent } from '../filter/filter.component';
+import { UnitCardComponent } from '../unit-card/unit-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FilterComponent],
+  imports: [FilterComponent, UnitCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/components/unit-card/unit-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unit-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './unit-card.component.html',
+  styleUrl: './unit-card.component.scss'
+})
+export class UnitCardComponent {
+
+}
